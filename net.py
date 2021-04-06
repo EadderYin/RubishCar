4a46396f2563afbfe71c126f96a60caf55c6cf2f
zrzz <zhangrenzhongren@163.com>
2021-04-06 11:55:28 +0800

    修复了部分bug

--- net.py
+++ net.py
@@ -4,7 +4,7 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, TensorBoard
 import numpy as np
 from sklearn.model_selection import train_test_split
-import datatime
+import datetime
 
 import tensorboard
 
