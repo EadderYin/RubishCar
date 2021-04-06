535dfda14b66d14780f79c40435131c811fa4cc1
zrzz <zhangrenzhongren@163.com>
2021-04-06 23:42:40 +0800

    修改了setDataset.py

--- setDataset.py
+++ setDataset.py
@@ -1,22 +1,20 @@
 # %matplotlib inline
 # import tensorflow as tf
 import numpy as np
-import matplotlib.pylab as plt
-import pandas as pd
+import cv2 as cv
+# import matplotlib.pylab as plt
+# import pandas as pd
 import json
-import 
 
 #导入数据
 URL = './data/annotations.json'
 with open(URL,"r") as f:
     dataframe = json.load(f)
-imgrects = dataframe["annotations"]
-for imgrect in imgrects:
-    # print(str(imgrect["bbox"])+'\n')
-    if bbox is None:
-        bbox = tf.constant(imgrect["bbox"],dtype=tf.float32,shape=[1,1,4])
-
-
-# dataframe = pd.read_csv(URL)
-# dataframe.head()
+annotations = dataframe["annotations"]
+images = dataframe["images"]
+for annotation in annotations:
+    image_id = annotation["image_id"]
+    category_id = annotation["category_id"]
+    image_path = images[image_id]["file_name"]
+    print(image_path)
 
