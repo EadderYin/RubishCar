ec95815007bab295a9ddad2d2f6ff33f4df09c7c
zrzz <zhangrenzhongren@163.com>
2021-04-09 19:06:33 +0800

    保存了数据集中的图片

--- testDataset.py
+++ testDataset.py
@@ -1,11 +1,15 @@
 import cv2 as cv
 import numpy as np
+import os
 
 x = np.load('./data/x.npy')
 y = np.load('./data/y.npy')
 
 for i in range(0,len(x)):
-    print(y[i])
-    cv.imshow('IMG',x[i])
-    if cv.waitKey() == 13:
-        break
+    print(str(i)+':'+str(y[i]))
+    # cv.imshow('IMG',x[i])
+    if not cv.imwrite('./data/img/'+str(y[i])+'/'+str(i)+'.bmp',x[i]):
+        os.mkdir('./data/img/'+str(y[i]))
+        cv.imwrite('./data/img/'+str(y[i])+'/'+str(i)+'.bmp',x[i])
+    # if cv.waitKey() == 13:
+        # break
